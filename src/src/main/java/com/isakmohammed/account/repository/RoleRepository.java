466f4e1d871a726c80c01f9686de9acfe220d950
isak-mohammed <70150386+isak-mohammed@users.noreply.github.com>
2021-04-25 18:45:46 +0100

    Delete src directory

--- src/src/main/java/com/isakmohammed/account/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.isakmohammed.account.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.isakmohammed.account.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long>{
-}
