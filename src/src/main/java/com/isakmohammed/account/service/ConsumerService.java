f7330115759a3d6eace62f4437decbe8b3d5966b
isak-mohammed <70150386+isak-mohammed@users.noreply.github.com>
2021-04-25 18:52:23 +0100

    Delete src directory

--- src/src/main/java/com/isakmohammed/account/service/ConsumerService.java
+++
@@ -1,6 +0,0 @@
-package com.isakmohammed.account.service;
-
-public interface ConsumerService {
-
-    void consumerMessage(byte[] data);
-}
--- src/src/main/java/com/isakmohammed/account/service/ProducerService.java
+++
@@ -1,6 +0,0 @@
-package com.isakmohammed.account.service;
-
-public interface ProducerService {
-
-    public String  produceMessage(String message);
-}
